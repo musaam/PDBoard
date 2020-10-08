e55474b4a6d9ec916c34655e080a24d853cf1a56
Aminu <amin.gumel@gmail.com>
2020-10-08 08:33:14 -0500

    Adding an API

--- src/containers/PDBoard/PDBoard.js
+++ src/containers/PDBoard/PDBoard.js
@@ -1,7 +1,25 @@
 import classes from './PDBoard.module.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const PDBoard = (props) => {
+
+    const getMessage = async () => {
+        const response = await fetch(`/api/GetMessage`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+        const body = await response.json();
+        console.log(body);
+    };
+
+    useEffect(() => {
+        getMessage();
+        console.log('board loaded');
+    }, []);
+
+
     return (
         <div className={classes.PDBoard} >
             <h4>PD Board</h4>
